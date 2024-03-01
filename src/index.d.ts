5ed4ae36fca4efb6ea9b78c781fdb077bef734fb
Madhusudhan <Madhusudhan1863@gmail.com>
2024-03-01 18:07:09 +0530

    fix: custom event listener type issues

---
+++ src/index.d.ts
@@ -0,0 +1,21 @@
+interface CustomEventMap {
+  query: CustomEvent<{
+    pendingQuery: string;
+    queryName: string;
+  }>;
+}
+declare global {
+  interface Window {
+    addEventListener<K extends keyof CustomEventMap>(
+      type: K,
+      listener: (this: Document, ev: CustomEventMap[K]) => void
+    ): void;
+    dispatchEvent<K extends keyof CustomEventMap>(ev: CustomEventMap[K]): void;
+    removeEventListener<K extends keyof CustomEventMap>(
+      type: K,
+      listener: (this: Document, ev: CustomEventMap[K]) => void
+    ): void;
+  }
+}
+
+export {};
