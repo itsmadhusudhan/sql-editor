32ddcbdb5218799b5f3a6d365769ff627b12a6d1
Madhusudhan <Madhusudhan1863@gmail.com>
2024-03-02 10:01:20 +0530

    refactor: table creation

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -6,44 +6,43 @@ import borrowers from "@/db/borrowers.json";
 import genres from "@/db/genres.json";
 import loans from "@/db/loans.json";
 
-export const seedData = () => {
-  alasql("DROP TABLE IF EXISTS authors");
-  alasql(`CREATE TABLE authors (
-      author_id int(11) NOT NULL AUTO_INCREMENT,
-      first_name varchar(255) NOT NULL,
-      last_name varchar(255) NOT NULL,
-      PRIMARY KEY (author_id)
-    )`);
-
-  alasql("DROP TABLE IF EXISTS `genres`;");
-  alasql(`CREATE TABLE genres (
+export const tableSchemas = {
+  drop: [
+    "DROP TABLE IF EXISTS authors",
+    "DROP TABLE IF EXISTS genres",
+    "DROP TABLE IF EXISTS books",
+    "DROP TABLE IF EXISTS borrowers",
+    "DROP TABLE IF EXISTS loans",
+  ],
+  create: [
+    `CREATE TABLE authors (
+    author_id int(11) NOT NULL AUTO_INCREMENT,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    PRIMARY KEY (author_id)
+  )`,
+    `CREATE TABLE genres (
     genre_id int(11) NOT NULL AUTO_INCREMENT,
-    name varchar(255) NOT NULL,
+    name text NOT NULL,
     PRIMARY KEY (genre_id)
-  )`);
-
-  alasql("DROP TABLE IF EXISTS `books`");
-  alasql(`CREATE TABLE books (
-      book_id int(11) NOT NULL AUTO_INCREMENT,
-      title varchar(255) NOT NULL,
-      author_id int(11) NOT NULL,
-      genre_id int(11) NOT NULL,
-      publication_year int(11) DEFAULT NULL,
-      PRIMARY KEY (book_id),
-      FOREIGN KEY(author_id) REFERENCES authors(author_id),
-      FOREIGN KEY(genre_id) REFERENCES genres(genre_id)
-    )`);
-
-  alasql("DROP TABLE IF EXISTS `borrowers`;");
-  alasql(`CREATE TABLE borrowers (
-      borrower_id int(11) NOT NULL AUTO_INCREMENT,
-      name varchar(255) NOT NULL,
-      contact_info varchar(255) DEFAULT NULL,
-      PRIMARY KEY (borrower_id)
-    )`);
-
-  alasql("DROP TABLE IF EXISTS loans;");
-  alasql(`CREATE TABLE loans (
+  )`,
+    `CREATE TABLE books (
+    book_id int(11) NOT NULL AUTO_INCREMENT,
+    title text NOT NULL,
+    author_id int(11) NOT NULL,
+    genre_id int(11) NOT NULL,
+    publication_year int(11) DEFAULT NULL,
+    PRIMARY KEY (book_id),
+    FOREIGN KEY(author_id) REFERENCES authors(author_id),
+    FOREIGN KEY(genre_id) REFERENCES genres(genre_id)
+  )`,
+    `CREATE TABLE borrowers (
+    borrower_id int(11) NOT NULL AUTO_INCREMENT,
+    name text NOT NULL,
+    contact_info text DEFAULT NULL,
+    PRIMARY KEY (borrower_id)
+  )`,
+    `CREATE TABLE loans (
     loan_id int(11) NOT NULL AUTO_INCREMENT,
     book_id int(11) NOT NULL,
     borrower_id int(11) NOT NULL,
@@ -51,7 +50,17 @@ export const seedData = () => {
     return_date date DEFAULT NULL,
     FOREIGN KEY(book_id) REFERENCES books(book_id),
     FOREIGN KEY(borrower_id) REFERENCES borrowers(borrower_id)
-  )`);
+  )`,
+  ],
+};
+
+export const seedData = () => {
+  tableSchemas.drop.forEach((query) => {
+    alasql(query);
+  });
+  tableSchemas.create.forEach((query) => {
+    alasql(query);
+  });
 
   alasql.tables.authors.data = authors;
   alasql.tables.books.data = books;
